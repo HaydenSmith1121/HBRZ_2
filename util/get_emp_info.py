728508b6a5dad8abd9347108e4814d3b49d09076
赵志成 <2962817993@qq.com>
2026-02-28 14:45:49 +0800

    更新

--- util/get_emp_info.py
+++ util/get_emp_info.py
@@ -13,34 +13,23 @@ session.headers.update({
     "Accept": "application/json, text/plain, */*",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-    "Referer": "http://192.168.1.25/pfmcManage/pfmcenvironment/pfmcOrg?isExternalUser=false",
     "menuCode": "8712698855247806464",
     "moduleCode": ""
 })
 
 resp = session.get("http://192.168.1.25/api/pfmc-asist/pfmcOrgEmp/listEmpByOrgId?pageNum=1&pageSize=10")
 
-# print(resp.content)
-# print(resp.encoding)
 print(resp.json())
-# data1=resp.json()["data"]
-# info=data1["list"]
-# # print(len( info))
-# # print(len(data1))
-# #
-# # with open("../res/emp_info.json", "w", encoding="utf-8") as fp:
-# #     json.dump(resp.json(), fp, ensure_ascii=False)
-# #     fp.write("\n")
-#
-# with open("../res/emp_info_1.csv", "w", encoding="utf-8", newline="") as f:
-#     writer=csv.writer(f)
-#     writer.writerow(["姓名","工号","密码"])
-#     for i in range(len(info)):
-#         name=info[i]["empName"]
-#         empNo=info[i]["empNumber"]
-#         writer.writerow([name,empNo,""])
+resp_data = resp.json()["data"]
+data_list = resp_data["list"]
 
-
-
+with open("../res/emp_info_1.csv", "w", encoding="utf-8", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["工号", "姓名 ", "部门", "密码"])
+    for i in range(len(data_list)):
+        empNumber = data_list[i]["empNumber"]
+        empName = data_list[i]["empName"]
+        orgFullName = data_list[i]["orgFullName"]
+        writer.writerow([empNumber, empName, orgFullName, ""])
 
 session.close()
